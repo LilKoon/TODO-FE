b15a6ec599cce552c01f421a7e7d0fbf723f8af4
LilKoon <ngdcmanh2004@gmail.com>
2026-01-14 15:43:57 +0700

    cap nhat lai todo va weather

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,33 @@
 import { reactRouter } from "@react-router/dev/vite";
-import path from "path"
+import { vercelPreset } from "@vercel/react-router/vite";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     },
   },
 });
